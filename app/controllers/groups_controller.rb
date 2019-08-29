0524314d6ef4dc7342cc278a2bc9b03efd68fe3d
JyunpeiKuroda <totoro.black6996@gmail.com>
2019-08-29 19:33:58 +0900

    editのビュー崩れ修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,12 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
   end
 
   def new
     @group = Group.new
-    @group.users << current_user    
+    @group.users << current_user
   end
 
   def create
